393ed79f1ccde0157582f77baf73fdf0a6dde98e
Mizoo1 <mouaz-11@hotmail.com>
2023-10-04 07:22:32 +0200

    All other API of Product"29"

--- src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
+++ src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,5 +53,36 @@ public class ProductRestImpl implements ProductRest {
             ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> deleteProduct(Integer id) {
+        try{
+            return productService.deleteProduct(id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> updateStatus(Map<String, String> requestMap) {
+        try{
+            return productService.updateStatus(requestMap);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<ProductWrapper>> getByGategory(Integer id) {
+        try{
+            return productService.getByGategory(id);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
+++ src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
@@ -37,4 +37,8 @@ public class ProductWrapper {
         this.categoryName = categoryName;
     }
 
+    public ProductWrapper(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
